d77f63a1d88c73aec44984b827d00178813021b5
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-03-07 15:26:22 -0500

    style components

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,37 +8,54 @@ class Product extends Component {
         return(
             <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={()=>console.log("click me")}></div>
-                
-                <Link to="/details">
-
-                <img src={img} alt="product img" className="card-img-top" />
-                </Link>
-
-                <button className="cart-btn" disabled={inCart?true:false} onClick={()=>{console.log("added to the cart")}}>
+              
 
 
-                {inCart ? (
-                    <p className="text-capitalize mb-0" disabled>
-                      Item In Cart
-                    </p>
-                  ) : (
-                    <i className="fa fa-cart-plus" />
-                  )}
+
+
+                <div className="img-container p-5" onClick={()=>console.log("click me")}>
                 
-                
-                
-                
-                
-                
-                
-                
-                </button>
+                    <Link to="/details">
+
+                        <img src={img} alt="product img" className="card-img-top" />
+                    </Link>
+
+
+                      <button className="cart-btn small-btn float-right"  disabled={inCart?true:false} onClick={()=>{console.log("added to the cart")}}>
+
+
+                            {inCart ? (
+                                <p className="text-capitalize mb-0" disabled>
+                                Item In Cart
+                                </p>
+                                    ) : (
+                                        <i className="fa fa-cart-plus" />
+                                    )}
+
+
+        </button>
+                </div>
+              
                 </div>
 
+                {/* card footer */}
+
+                  <div className="card-footer d-flex justify-content-between bblock">
+
+                    <h5><p className="align-self-center mb-0 titlecard ">{title}</p></h5>
+
+                    <h5 className="mb-0 price">
+
+                        <span className="mr-1">$</span> {price}
+                    </h5>
+
+
+
+                   </div> 
+
+                    
 
                 
-                {/* <h3>Product Component</h3> */}
             </div>
         )
     }
